d3a9a392d6c8a44c6d061bc198ccfc60ff624385
Abdullah Ba Nafea <abu.hd3@gmail.com>
2025-12-27 01:15:13 +0800

    feat: add queue worker for emails

--- app/Mail/OtpCodeMail.php
+++ app/Mail/OtpCodeMail.php
@@ -3,14 +3,16 @@
 namespace App\Mail;
 
 use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class OtpCodeMail extends Mailable
+class OtpCodeMail extends Mailable implements ShouldQueue
 {
-    use SerializesModels;
+    use Queueable, SerializesModels;
 
     public string $otp;
     public string $purpose;
